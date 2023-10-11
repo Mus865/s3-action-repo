86a0fd085c8c365f024280d2831ff6ab8ccdaa06
mustapha garba <musgab3@gmail.com>
2023-10-11 18:54:50 -0400

    backend

--- maint.tf
+++ maint.tf
@@ -1,5 +1,23 @@
 module "s3" {
-    source = "git@github.com:Mus865/s3-backend.git//s3-module?ref=v1.0.1"
-  
+  source = "git@github.com:Mus865/s3-backend.git//s3-module?ref=v1.1.0"
+
 }
-  
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket  = "mustaphkops"
+    region  = "us-east-1"
+    key     = "action/terraform.tfstate"
+    encrypt = true
+
+  }
+}
+
+
+
