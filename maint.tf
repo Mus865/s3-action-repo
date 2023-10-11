34794671e3c1d3c2d6e7eba004a1bceaefcac1cc
mustapha garba <musgab3@gmail.com>
2023-10-10 22:03:36 -0400

    fix:commit

--- maint.tf
+++ maint.tf
@@ -1,5 +1,5 @@
 module "s3" {
-    source = "git::ssh://git@github.com:Mus865/s3-backend.git//s3-module?ref=v1.0.1"
+    source = "git@github.com:Mus865/s3-backend.git//s3-module?ref=v1.0.1"
   
 }
   
